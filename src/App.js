0caf47f455771e30568221f65f971e9008571a0c
MD <mdundukov@gmail.com>
2023-04-19 19:23:53 -0600

    interval Date format

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
 import './App.css';
 import {Component} from "react";
-import {UserList} from "./components/userList";
-import {SelectedUserList} from "./components/SelectedUserList";
-
-const userDB = [
-    {id: 1, name: 'James', lastName: 'Edisson'},
-    {id: 2, name: 'Jonas', lastName: 'Jonason'},
-    {id: 3, name: 'Makil', lastName: 'Ratnik'},
-    {id: 4, name: 'Tron', lastName: 'Copit'},
-    {id: 5, name: 'Robert', lastName: 'Leonsorn'},
-    {id: 6, name: 'Endy', lastName: 'Frankovsky'}];
+import {StopWatch} from "./components/StopWatch";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: userDB.map((user) => ({...user, isSelected: false})),
-        }
-    }
-
-    setUsers = (newUsers) => this.setState({users: [...newUsers]});
-
     render() {
-        const {users} = this.state;
         return (
             <>
                 <header>
-                    <SelectedUserList users={[...users]}/>
+                    <h1>Stop Watch</h1>
                 </header>
                 <main>
-                    <UserList users={users} setUsers={this.setUsers}/>
+                    <StopWatch/>
                 </main>
             </>)
     }
---
+++ src/components/StopWatch/index.jsx
@@ -0,0 +1,58 @@
+import {Component} from "react";
+
+export class StopWatch extends Component {
+    constructor(props) {
+        super(props);
+        this.intervalId = null;
+        this.state = {
+            time: new Date(0, 0, 0, 0, 0, 0, 0)
+        }
+    }
+
+    tick = () => {
+        const {time} = this.state;
+        time.setSeconds(time.getSeconds() + 1);
+        this.setState({time});
+    };
+
+    start = () => {
+        if (!this.intervalId) {
+            this.intervalId = setInterval(this.tick, 1000);
+        }
+    }
+    stop = () => {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+    reset = () => {
+        this.stop();
+        this.setState({time: new Date(0, 0, 0, 0, 0, 0, 0)});
+    };
+
+    componentDidMount() {
+        // this.start();
+    }
+
+    componentWillUnmount() {
+        this.stop();
+    }
+
+    render() {
+        const {time} = this.state
+        return (<article>
+            <div>This state Count: {time.toLocaleTimeString()}</div>
+            <button onClick={() => {
+                this.start();
+            }}>Start
+            </button>
+            <button onClick={() => {
+                this.stop();
+            }}>Stop
+            </button>
+            <button onClick={() => {
+                this.reset();
+            }}>Reset
+            </button>
+        </article>);
+    }
+}
