9e619676c6cacdde77733dc41a63360dcfa381d7
MD <mdundukov@gmail.com>
2023-04-22 15:36:21 -0600

    FlexContainer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import {Component} from "react";
-import {ImageWrapper} from "./components/ImageWrapper";
+import {FlexContainer} from "./components/FlexContainer";
 
 class App extends Component {
     render() {
         return (
             <>
-                <ImageWrapper width={'500px'} height={'700px'}>
-                    <img
-                        src='https://img3.akspic.ru/previews/6/4/4/1/7/171446/171446-verholaz-chudo_chelovek_pauk_majl_morales-chelovek_pauk-majlz_morales-komiksy_marvel-x750.jpg'
-                        alt=""/>
-                </ImageWrapper>
+                <FlexContainer justifyContent={'space-around'} alignItems={'center'}>
+                    <div>Test</div>
+                    <div>Test</div>`
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                </FlexContainer>
             </>)
     }
 }
---
+++ src/components/FlexContainer/index.jsx
@@ -0,0 +1,19 @@
+import styles from './FlexContainer.module.scss'
+import PropTypes from "prop-types";
+
+export const FlexContainer = (props) => {
+    const {children, alignItems, justifyContent} = props;
+    const flexStyle = {
+        justifyContent,
+        alignItems
+    }
+    return <div style={flexStyle} className={styles.flexContainer}>
+        {children}
+    </div>
+}
+
+FlexContainer.propsType = {
+    children: PropTypes.element.isRequired,
+    justifyContent: PropTypes.string.isRequired,
+    alignItem: PropTypes.string.isRequired,
+}
