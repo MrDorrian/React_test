4dd542ff013bf991e55094c4371526b3c79d5515
MD <mdundukov@gmail.com>
2023-04-24 17:21:47 -0600

    Add nav route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Component } from 'react';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import {Route, BrowserRouter, Routes, Link} from 'react-router-dom';
 import UserLoader from './components/UsersLoader';
 import Calendar from './components/Calendar';
 import Aloha from './components/greeting';
@@ -9,6 +9,19 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/calendar">Calendar</Link>
+            </li>
+            <li>
+              <Link to="/aloha">Aloha</Link>
+            </li>
+          </ul>
+        </nav>
         <Routes>
           <Route index element={<UserLoader />} />
           <Route path="/calendar" element={<Calendar />} />
