e0f1608f4adc5c4aa7f738179cd292850407ca28
Dorrian <mdundukov@gmail.com>
2023-05-03 22:13:13 -0600

    Context type for class

--- src/components/TestTaskPage/Header/index.jsx
+++ src/components/TestTaskPage/Header/index.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import { Component } from 'react';
 import style from './Header.module.scss';
 import { UserContext } from '../../../context/UserContext';
 
-export const Header = () => {
-  const getUserData = (user) => {
-    const { url, name } = user;
+// export const Header = () => {
+//   const getUserData = (user) => {
+//     const { url, name } = user;
+//     return (
+//       <div className={style.wrapper}>
+//         <div className={style.UserWrapper}>
+//           <img src={url} alt={name} className={style.userProfile} />
+//           <p className={style.UserName}>{name}</p>
+//         </div>
+//       </div>
+//     );
+//   };
+//
+//   return <UserContext.Consumer>{getUserData}</UserContext.Consumer>;
+// };
+
+export class Header extends Component {
+  render() {
+    const user = this.context;
     return (
       <div className={style.wrapper}>
         <div className={style.UserWrapper}>
-          <img src={url} alt={name} className={style.userProfile} />
-          <p className={style.UserName}>{name}</p>
+          <img src={user.url} alt={user.name} className={style.userProfile} />
+          <p className={style.UserName}>{user.name}</p>
         </div>
       </div>
     );
-  };
+  }
+}
 
-  return <UserContext.Consumer>{getUserData}</UserContext.Consumer>;
-};
+Header.contextType = UserContext;
